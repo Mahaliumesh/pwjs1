19aaf3bd0b87812d80230c35fe635e105d140a36
Umesh mahali <124446552+Mahaliumesh@users.noreply.github.com>
2023-11-05 15:07:56 +0000

    discuss about variables

--- basics1/test.js
+++ basics1/test.js
@@ -1,1 +1,5 @@
 console.log("hitesh")
+let Email = "umeshmahali418@gmail.com"
+let password = "123456789"
+console.log(Email)
+console.log(password)
---
+++ basics1/variables.js
@@ -0,0 +1,11 @@
+//variables - let
+//Constants - const
+
+let userEmail = "hitesh@hc.com"
+//let user_password = "123"
+let password = "123"
+//let 1userpassword = "123" //not allowed
+console.log(userEmail)
+console.log(password)
+
+
