3a81af5f9af88b19a9aba73a1e30e40af3cecdec
Umesh mahali <124446552+Mahaliumesh@users.noreply.github.com>
2023-11-05 18:01:00 +0000

    discuss about data-type

---
+++ basics1/data-type.js
@@ -0,0 +1,38 @@
+//primitive
+/*1. String " "    const firstname = "Satvik"
+2. Number        const age = 20  "or"  const score = 200               
+3. Boolean       let isLoggedIn = true 
+4. Null          const nodeversion = Null     
+5. Undefined     let bankbalance = Undefined
+6. Symbol        const buttonId = Symbol("Id")
+7. BigInt        const ReallyLongNumber = 900713344n
+
+2+2=4 -> int(Number)
+"2"+"2"=22  ->  String
+*/
+
+
+//Reference (Non-primitive) - Objects
+
+/*
+Arrays
+Objects
+Functions
+*/ 
+
+
+
+//Arrays
+const numbers = [1,2,3,4,5]                          //big_bracket is Arrays []
+const heroes = ["superman", "batman", "flash"]
+
+
+
+//Objects
+const userObject = {                                   //curly_bracket is Objects {}
+    name: "hitesh",
+    age: 21,
+    isLoggedIn: true
+}
+
+//Functions
